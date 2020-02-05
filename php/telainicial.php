334865b8ce75bcc955781993d5017eab9832541b
nicoly <nicolymenezees@gmail.com>
2020-02-04 22:39:05 -0300

    já esta aparecendo o tweet

--- php/telainicial.php
+++ php/telainicial.php
@@ -31,22 +31,31 @@
 <div class="img-fluid" style="position: relative;">
     <img src="background.jpg" class="img-fluid" style="position:absolute; z-index: 1;" alt="Responsive image">
         <div style="border-radius: 730px;"width="35" height="30" >
-            <img src="perfil.jpg" style="position:absolute; left:1px; top:242px; left: 140px; z-index: 2; border-radius: 50%; height: 210px; width: 210px;" alt="...">
+          <img src="perfil.jpg" style="position:absolute; left:1px; top:242px; left: 140px; z-index: 2; border-radius: 50%; height: 210px; width: 210px;" alt="...">
+        </div>
+        <div style="width: 700px;">
+          <button class="btn btn-primary btn-sm " style="border-radius: 7%; color:white; font-weight: bold; position:absolute; width: 100px; top: 418px; left: 28%; z-index: 3;" type="submit">Editar</button>
         </div>
 </div>
 
-
+<form action="#" method="POST" target="_self">
 <div  class="container" style=" background-color : #343a40;">
 <div class="input-group" style="position: relative; top: 480px; width: 100%; margin-right: auto; margin-left: auto;" >
-  <input style="height: 100px; border: 1px solid  rgba(21, 32, 43, 0.5);" type="text" class="form-control" placeholder="O que está acontecendo?" aria-label="Recipient's username with two button addons" aria-describedby="button-addon4">
-  <div class="input-group-append" id="button-addon4">
-    <button class="btn btn-primary" style="height: 40px; top: 30%; left: 10%;" type="button" style="border-radius: 30%;">Tweetar</button>
+  <input  type="text"   name="tweetar" style="height: 100px; border: 1px solid  rgba(21, 32, 43, 0.5); font-size: 21px;" class="form-control" placeholder="O que está acontecendo?" aria-label="Recipient's username with two button addons" aria-describedby="button-addon4">
+  <?php $tweet =($_POST['tweetar']) ? $_POST['tweetar'] : '';
+
+ 
+     ?>
+  <div class="input-group-append"   id="button-addon4">
+   <button class="btn btn-primary" style="height: 40px; top: 30%; left: 10%; border-radius: 7px;" type="submit">Tweetar</button>
+   
   </div>
+ 
+</div>
 </div>
 
+</form>
 
-
-</div>
 
 
 <ul class="nav nav-pills nav-fill" style="position: relative;  width: 100%; top: 530px; border: 0px 0px 1px 0px solid #ccc; border-radius: 10px;">
@@ -74,8 +83,12 @@
   <div class="card-body" style=" color: white;">
             <img src="perfil.jpg" style="position:relative; left:1px; top:-5px; left: 0px; z-index: 2; border-radius: 50%; height: 40px; width: 40px;" alt="...">
             <span style="font-weight: bold; margin-left: 7px;">Nicoly Jobs</span>
-    <h5 style="margin-top: 5px;" class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <h5 style="margin-top: 5px;" class="card-title"></h5>
+     <?php
+      echo $tweet;
+    ?>
+    <p class="card-text" ></p>
+   
   </div>
 </div>
 
